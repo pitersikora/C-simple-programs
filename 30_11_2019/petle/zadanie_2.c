3a2e85ff7a4e747efbf9770b0fffac416506bb87
Wlodek Bzyl <matwb@ug.edu.pl>
2019-11-30 12:00:26 +0100

    Nowa wersja zadania 2 w pętlach

--- 30_11_2019/petle/zadanie_2.c
+++ 30_11_2019/petle/zadanie_2.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-void print_row_1 ()
+void print_pattern();
+void print_layer();
+void print_row_1();
+void print_row_2();
+
+int main()
+{
+  print_pattern(4);
+}
+
+// ----
+
+void print_pattern(int layers) {
+  for (int i = 0; i < layers; i++)
+  {
+    print_layer();
+  }
+}
+
+void print_row_1()
 {
   printf("* * * * * * * *\n");
 }
 
-void print_row_2 ()
+void print_row_2()
 {
   printf(" * * * * * * * *\n");
 }
 
-int main()
-{
-  int rows = 4;
-
-  for (int i = 1; i < rows; i++)
-  {
-    print_row_1();
-    print_row_2();
-  }
-
+void print_layer() {
   print_row_1();
+  print_row_2();
 }
+
