9e26d95716897e5ccd486418591e2a24177ea6eb
pitersikora <piootrsikora@gmail.com>
2019-12-07 08:54:59 +0100

    [Loops] zadanie_8 comments added

--- 30_11_2019_loops/loops/zadanie_8.c
+++ 30_11_2019_loops/loops/zadanie_8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
 count is how many first symbol print at start
-print toggle is adding (1) or reducing (0) count in next layer
+print toggle is adding (1) or reducing (0) sign_1 count in next layer
 sign_1 is first symbol to print in layer
 sign_2 is second symbol to print in layer
 */
@@ -18,12 +18,14 @@ int main()
 void print_pattern(int count, int print_toggle, char sign_1, char sign_2)
 {
   printf("\n");
+  // one loop is one pattern layer, number of all signs in layer is 4
   for(int c = 0; c < 4; c++)
   {
     for(int i = 0; i < count; i ++)
     {
       putchar(sign_1);
     }
+    //print sign_2 to fill up layer
     for(int i = 0; i < 4 - count; i ++)
     {
       putchar(sign_2);
