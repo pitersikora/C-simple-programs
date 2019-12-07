27ac082a60d1d4d8ccad3f6e0b6a071b8d059368
pitersikora <piootrsikora@gmail.com>
2019-12-07 07:49:31 +0100

    [Loops] zadanie_2 renamed variables

--- 30_11_2019_loops/loops/zadanie_2.c
+++ 30_11_2019_loops/loops/zadanie_2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void print_pattern(int layers);
-void print_layer(int ammount, char sign);
-void print_row_1(int symbol_ammount, char symbol);
-void print_row_2(int symbol_ammount, char symbol);
+void print_layer(int ammount, char symbol);
+void print_row_1(int row_ammount, char row_symbol);
+void print_row_2(int row_ammount, char row_symbol);
 
 int main()
 {
@@ -20,27 +20,27 @@ void print_pattern(int layers)
   }
 }
 
-void print_row_1(int symbol_ammount, char symbol)
+void print_row_1(int row_ammount, char row_symbol)
 {
-  for(int i = 0; i < symbol_ammount; i++)
+  for(int i = 0; i < row_ammount; i++)
   {
-    printf("%c ", symbol);
+    printf("%c ", row_symbol);
   }
   printf("\n");
 }
 
-void print_row_2(int symbol_ammount, char symbol)
+void print_row_2(int row_ammount, char row_symbol)
 {
   printf(" ");
-  for(int i = 0; i < symbol_ammount; i++)
+  for(int i = 0; i < row_ammount; i++)
   {
-    printf("%c ", symbol);
+    printf("%c ", row_symbol);
   }
   printf("\n");
 }
 
-void print_layer(int ammount, char sign)
+void print_layer(int ammount, char symbol)
 {
-  print_row_1(ammount, sign);
-  print_row_2(ammount, sign);
+  print_row_1(ammount, symbol);
+  print_row_2(ammount, symbol);
 }
