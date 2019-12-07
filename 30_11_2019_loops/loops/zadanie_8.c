341107307f5954643e01cb68ca7dcab1f90c6a45
pitersikora <piootrsikora@gmail.com>
2019-12-07 09:05:57 +0100

    [Loops] added generic number of signs in layer

--- 30_11_2019_loops/loops/zadanie_8.c
+++ 30_11_2019_loops/loops/zadanie_8.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 /*
-count is how many first symbol print at start
+base is how many signs are in one layer
+count is how many sign_1 print at start
 print toggle is adding (1) or reducing (0) sign_1 count in next layer
 sign_1 is first symbol to print in layer
 sign_2 is second symbol to print in layer
 */
-void print_pattern(int count, int print_toggle, char sign_1, char sign_2);
+void print_pattern(int base, int count, int print_toggle, char sign_1, char sign_2);
 
 int main()
 {
-  print_pattern(1,1,'*',' ');
-  print_pattern(4,0,'*',' ');
-  print_pattern(0,1,' ','*');
-  print_pattern(3,0,' ','*');
+  print_pattern(4,1,1,'*',' ');
+  print_pattern(4,4,0,'*',' ');
+  print_pattern(4,0,1,' ','*');
+  print_pattern(4,3,0,' ','*');
 }
 
-void print_pattern(int count, int print_toggle, char sign_1, char sign_2)
+void print_pattern(int base, int count, int print_toggle, char sign_1, char sign_2)
 {
   printf("\n");
-  // one loop is one pattern layer, number of all signs in layer is 4
-  for(int c = 0; c < 4; c++)
+  // one loop is one pattern layer, all symbols in layer are equal to base
+  for(int c = 0; c < base; c++)
   {
     for(int i = 0; i < count; i ++)
     {
       putchar(sign_1);
     }
     //print sign_2 to fill up layer
-    for(int i = 0; i < 4 - count; i ++)
+    for(int i = 0; i < base - count; i ++)
     {
       putchar(sign_2);
     }
